f05c5bbed88f1c0ca5eb0949e7c9857405359ecc
monikashah <monikapatira@gmail.com>
2023-03-29 11:49:47 +0530

    updated settings.py

--- forms/forms.py
+++ forms/forms.py
@@ -1,6 +1,6 @@
 from django import forms 
 from .models import BasicInformationModel #NutrigardenInformationModel, factsheetInformationModel, factsheetDynamicIndicatorsModel, financialExpensesModel, outputIndicatorsModel
-from bootstrap_datepicker_plus.widgets import DateTimePickerInput
+# from bootstrap_datepicker_plus.widgets import DateTimePickerInput
 
 class BasicInformationForm(forms.ModelForm):
     class Meta:
